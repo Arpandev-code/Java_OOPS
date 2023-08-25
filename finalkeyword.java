a0205d12996fae1738c7082176cd71c9038f13cd
Arpandev <arpan.dev2016@gmail.com>
2023-08-25 09:51:41 +0530

    New Files

---
+++ finalkeyword.java
@@ -0,0 +1,22 @@
+class Cal{
+    public void show(){
+        System.out.println("Made by Arpandev");
+    }
+    public void add(int a, int b){
+        System.out.println(a+b);
+    }
+
+}
+
+
+
+public class finalkeyword {
+    public static void main(String[] args) {
+    
+    //final keyword on variable 
+    //    final int x=0; // making variable final means you can't change the 
+    //     x=9;
+    //     System.out.println(x);
+    
+    }
+}
