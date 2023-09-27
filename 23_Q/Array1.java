2bbd1db888598bc8ff56d97a7298d9174d63d298
Arpandev <arpan.dev2016@gmail.com>
2023-09-27 14:30:50 +0530

    Added Interview Questions

---
+++ 23_Q/Array1.java
@@ -0,0 +1,9 @@
+class Array1{
+    public static void main(String[] args) {
+        int arr []=new int[6];
+        for(int a:arr)
+        {
+            System.out.println(a);
+        }
+    }
+}
